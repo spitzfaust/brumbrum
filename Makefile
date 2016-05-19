9c5f7f6869028c830b4b8695b2ecbc6e5669add2
pommi15 <pommi15@hotmail.com>
2016-05-19 18:06:41 +0200

    started shit

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@
 CC=gcc
 CFLAGS=-g -Wall
 
-all: testsignal myfifo mypipe mypopen myqueue_client gridserver
+all: testsignal myfifo mypipe mypopen  gridserver
 
 testsignal: testsignal.o
 	${CC} ${CFLAGS} testsignal.o -o testsignal
@@ -28,19 +28,9 @@ mypopen: mypopen.o
 mypopen.o: mypopen.c
 	${CC} ${CFLAGS} -c mypopen.c -o mypopen.o
 
-myqueue_client: myqueue_client.o
-	${CC} ${CFLAGS} myqueue_client.o -o myqueue_client
-
-myqueue_client.o: myqueue_client.c
-	${CC} ${CFLAGS} -c myqueue_client.c -o myqueue_client.o
-
 gridserver: gridserver.o
 	${CC} ${CFLAGS} gridserver.o -o gridserver
 
-myqueue_server.o: myqueue_server.c
-	${CC} ${CFLAGS} -c myqueue_server.c -o myqueue_server.o
-
-
 .PHONY: clean
 clean:
-	rm -f *.o myfifo mypipe mypopen myqueue_client myqueue_server
+	rm -f *.o myfifo mypipe mypopen  gridserver
--- gridserver.c
+++ gridserver.c
@@ -1,38 +1,64 @@
-#include "myqueue.h"
+#include "autobahn.h"
 
 int main(int argc, char* argv[])
 {
-   message_t msg;	/* Buffer fuer Message */
-   int msgid = -1;	/* Message Queue ID */
+  int height;
+  int width;
+  if((strcmp("-x", argv[1]) == 0) && (strcmp("-y", argv[3]) == 0)){
+    height = *argv[4];
+    width = *argv[2];
+  }else if((strcmp("-y", argv[1]) == 0) && (strcmp("-x", argv[3]) == 0)){
+    height = *argv[2];
+    width = *argv[4];
+  }
+  int size = 5 * 4;
 
-   /* Message Queue neu anlegen */
-   if( (msgid = msgget(KEY,PERM | IPC_CREAT | IPC_EXCL ))==-1 )
-   {
-      /* error handling */
-      fprintf(stderr,"%s: Error creating message queue\n",argv[0]);
+  char *grid;
+  grid = (char *) malloc( (size) * sizeof(char) );
+
+  for(int i = 0; i < size; ++i){
+    grid[i] = 'X';
+  }
+  printf("FUCK");
+  for(int y = 0; y < height; ++y){
+    for(int x = 0; x < width; ++x){
+      printf("%c", grid[y*(height - 1) + x]);
+    }
+    printf("\n");
+  }
+
+  navigation navi[26];	/* Buffer fuer navigation am grid */
+  int msgid = -1;	/* Message Queue ID */
+
+  /* Message Queue neu anlegen */
+  if( (msgid = msgget(KEY,PERM | IPC_CREAT | IPC_EXCL ))==-1 )
+  {
+    /* error handling */
+    fprintf(stderr,"%s: Error creating message queue\n",argv[0]);
+    return EXIT_FAILURE;
+  }
+
+
+  /* In einer Endlosschleife Nachrichten empfangen */
+  /*while (1)
+  {
+    if (msgrcv(msgid,&msg,sizeof(msg)-sizeof(long), 0 , 0) == -1)
+    {
+      // error handling
+      fprintf(stderr,"%s: Can't receive from message queue\n",argv[0]);
       return EXIT_FAILURE;
-   }
+    }*/
+    //printf("Message received: %s\n",msg.mText);
 
-   /* In einer Endlosschleife Nachrichten empfangen */
-   while (1)
-   {
-      if (msgrcv(msgid,&msg,sizeof(msg)-sizeof(long), 0 , 0) == -1)
-      {
-         /* error handling */
-         fprintf(stderr,"%s: Can't receive from message queue\n",argv[0]);
-         return EXIT_FAILURE;
-      }
-      printf("Message received: %s\n",msg.mText);
-      
-      /* sends message to griddisplay*//*
-      if (msgsnd(msgid,&msg,sizeof(msg)-sizeof(long), 0) == -1)
-      {
-            // error handling
-            fprintf(stderr,"%s: Can't send message\n",argv[0]);
-            return EXIT_FAILURE;
-      }
-      printf("Message sent: %s\n",msg.mText);
-      */
-   }
-   return EXIT_SUCCESS;
+    /* sends message to griddisplay*//*
+    if (msgsnd(msgid,&msg,sizeof(msg)-sizeof(long), 0) == -1)
+    {
+          // error handling
+          fprintf(stderr,"%s: Can't send message\n",argv[0]);
+          return EXIT_FAILURE;
+    }
+    printf("Message sent: %s\n",msg.mText);
+
+  }*/
+  return EXIT_SUCCESS;
 }
